cef1ea84a27557765f80b37c20db7769f0f1e6bd
khaledRahnama <157706646+khaledRahnama@users.noreply.github.com>
2024-07-24 12:22:59 +0330

    gridTemplateColumns

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
 
+const name = 'khaled';
+const x = 10
+const y = 20
+const names = ['khaled', 'ali', 'hasan']
+const families=['rahnama','something','sth']
+
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        Hello {name}
+      </h1>
+      <p> The sum of {x} and {y} is {x + y}</p>
+      <ul>
+        {names.map((names) => (
+          <li> {names} </li>
+        ))}
+      </ul>
+
+      <ul>
+        {families.map((families)=>(
+          <li> {families}</li>
+        ))}
+      </ul>
+
+    </div>
   )
 }
 
